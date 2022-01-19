b1770c010c144e7c97dff7f357c5104dc95865e8
Iakovos Tenedios <2519746t@student.gla.ac.uk>
2022-01-19 21:27:36 +0000

    Chapter 3.5 Checkpoint

---
+++ rango/urls.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 19 21:10:06 2022
+
+@author: Iacovos
+"""
+
+from django.urls import path
+from rango import views
+
+app_name = 'rango'
+
+urlpatterns = [
+    path('', views.index, name= 'index'),
+    ]
